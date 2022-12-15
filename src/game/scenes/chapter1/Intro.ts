c005ec726ffb58b2300353b168a68fc5d934f8dc
mishazawa <macintoshkrug@gmail.com>
2022-12-15 14:37:55 +0200

    add cli to generate scenes add tsconfig add core alias

---
+++ src/game/scenes/chapter1/Intro.ts
@@ -0,0 +1,34 @@
+import { PerspectiveCamera, Scene } from "three";
+import { Globals } from "core/globals";
+import { IOnDestroy, IOnInit } from "core/lifecycle";
+import { Scene as ShrehScene } from "core/Scene";
+
+export class Intro extends ShrehScene implements IOnInit, IOnDestroy {
+ constructor () {
+    super();
+
+    this.scene = new Scene();
+
+    this.camera = new PerspectiveCamera(
+      Globals.defaultCamera.fov,
+      Globals.aspect,
+      Globals.defaultCamera.near,
+      Globals.defaultCamera.far
+    );
+
+    this.camera.position.z = 5;
+  }
+
+  public override animate(delta: number) {
+
+  }
+
+
+  public onInit () {
+    return Promise.resolve()
+  }
+
+  public onDestroy () {
+    return Promise.resolve()
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
 import "./types.d";
-import { Core as App } from "./core";
+import { Core as App } from "core";
 import { TestScene } from "./game/scenes/test";
 import { TestSwapScene } from "./game/scenes/testswap";
-import { BallExampleScene } from "./game/scenes/ballexample";
 
 const canvas = document.querySelector("canvas.screen");
 
 const shreh = new App(canvas as HTMLCanvasElement);
 
-shreh.setScene(new BallExampleScene());
+shreh.setScene(new TestScene());
 
 shreh.animate();
 
-// setTimeout(() => {
-//   shreh.setScene(new TestSwapScene());
-// }, 5000)
+setTimeout(() => {
+  shreh.setScene(new TestSwapScene());
+}, 5000)
