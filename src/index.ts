8923af00c46d94b7b293ccb5ce2acccf16b81d10
mishazawa <macintoshkrug@gmail.com>
2022-12-16 12:56:46 +0200

    framework

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import "./types.d";
 import "./style/index.scss";
 
 import { Core as App } from "core";
-import { Intro } from './game/scenes/chapter1/Intro';
 
 
 const canvas = document.createElement('canvas');
@@ -12,7 +11,7 @@ document.body.appendChild(canvas);
 
 const shreh = new App(canvas as HTMLCanvasElement);
 
-shreh.setScene(new Intro()).then(() => {
-  shreh.animate();
-})
+// shreh.setScene(new Intro()).then(() => {
+//   shreh.animate();
+// })
 
